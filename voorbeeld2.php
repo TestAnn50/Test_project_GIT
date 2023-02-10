177957600a9763bc4f77a3fd535e670e6d292b0e
Ann Colpaert <ann.colpaert@gtibeveren.be>
2023-02-10 09:06:22 +0100

    aanpassing $a

--- voorbeeld2.php
+++ voorbeeld2.php
@@ -4,7 +4,7 @@ $link = new mysqli('localhost','root','Test1234','classicmodels');
 
 //2: als de verbinding gelukt is
 //even iets wijzigen om Git te testen
-$a = 'testing git';
+$a = 'testing git in de klas';
 if($link)
 {
     //3: opbouw van de query
